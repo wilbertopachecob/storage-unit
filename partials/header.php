34f3cdbc15cbc0cb6939685b53d7ead50675285b
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2025-09-13 21:44:15 -0500

    Fix Storage Unit navigation link to properly redirect to home page

--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="http://<?=$_SERVER['HTTP_HOST'] . '/storageUnit'?>">Storage Unit</a>
+  <a class="navbar-brand" href="<?=$_SERVER['PHP_SELF']?>">Storage Unit</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
