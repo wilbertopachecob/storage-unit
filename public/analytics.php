e2cd0bfd4ed9fea73010bd5e72f81d40ac0382fa
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2025-09-14 08:49:28 -0500

    Fix undefined function error in analytics page
    - Add missing helpers.php include to public/analytics.php
    - Resolves 'Call to undefined function unsetVariables()' error
    - Function is defined in app/Helpers/helpers.php but wasn't included
    - Analytics page now works correctly without fatal error

--- public/analytics.php
+++ public/analytics.php
@@ -8,6 +8,7 @@ session_start();
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/app/config.php';
 require_once __DIR__ . '/../config/app/constants.php';
+require_once __DIR__ . '/../app/Helpers/helpers.php';
 require_once __DIR__ . '/../app/Middleware/guards.php';
 
 // Check if user is logged in
