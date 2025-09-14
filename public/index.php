1371f49d5947b21e94642600e89548ccbfceed9b
Wilberto Pacheco Batista <wilbertopachecob@gmail.com>
2025-09-14 09:52:08 -0500

    Fix Items dropdown not working on index.php
    - Remove duplicate script loading (jQuery, Popper.js, Bootstrap) from index.php
    - Scripts are now loaded only once through footer.php to avoid conflicts
    - This resolves the dropdown functionality issue that was working on analytics.php but not index.php
    - Keep only custom JS files (card.js, main.js, upload-image-preview.js) in index.php

--- public/index.php
+++ public/index.php
@@ -101,42 +101,7 @@ include __DIR__ . '/../routes/routes.php';
 <?php
 include __DIR__ . '/../resources/views/footer.php';
 ?>
-<!-- Just for future references, NEVER USE JQUERY SLIM AGAIN, 
-it cause some problems with fadeIn, hide('slow') and more  -->
-<!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script>
-// Load Popper.js with fallback chain
-(function() {
-    const popperSources = [
-        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js',
-        'https://unpkg.com/popper.js@1.14.6/dist/umd/popper.min.js',
-        'js/popper.min.js'
-    ];
-    
-    function loadPopper(index = 0) {
-        if (index >= popperSources.length) {
-            console.error('All Popper.js sources failed to load');
-            return;
-        }
-        
-        const script = document.createElement('script');
-        script.src = popperSources[index];
-        script.crossOrigin = 'anonymous';
-        script.onload = function() {
-            console.log('Popper.js loaded from:', popperSources[index]);
-        };
-        script.onerror = function() {
-            console.warn('Failed to load Popper.js from:', popperSources[index]);
-            loadPopper(index + 1);
-        };
-        document.head.appendChild(script);
-    }
-    
-    loadPopper();
-})();
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<!-- Scripts are now loaded in footer.php to avoid duplication -->
 <script src="js/card.js?v=<?= time() ?>"></script>
 <script src="js/main.js?v=<?= time() ?>"></script>
 <script src="js/upload-image-preview.js?v=<?= time() ?>"></script>
